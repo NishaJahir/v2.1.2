4bbfe04c13df930dcde50c413dedca6799f9a251
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-20 18:11:00 +0530

    Update NovalnetPaymentMethodReinitializePaymentScript.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentScript.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentScript.php
@@ -4,14 +4,29 @@ namespace Novalnet\Providers\DataProvider;
 
 use Plenty\Plugin\Templates\Twig;
 use Novalnet\Helper\PaymentHelper;
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Plenty\Modules\Payment\Method\Models\PaymentMethod;
 
 class NovalnetPaymentMethodReinitializePaymentScript
 {
-  public function call(Twig $twig):string
+  public function call(Twig $twig, $arg):string
   {
-    
+    $order = $arg[0];
     $paymentHelper = pluginApp(PaymentHelper::class);
+    $paymentMethodRepository = pluginApp(PaymentMethodRepositoryContract::class);
     
-    return $twig->render('Novalnet::NovalnetPaymentMethodReinitializePaymentScript', ['mopIds' => ['paymentMethodId' => 6003]]);
+    $paymentMethods = $this->paymentMethodRepository->allForPlugin('plenty_novalnet');
+    
+    if(!is_null($paymentMethods))
+    {
+       $paymentMethodIds              = [];
+        foreach ($paymentMethods as $paymentMethod) {
+            if ($paymentMethod instanceof PaymentMethod) {
+                $paymentMethodIds[] = $paymentMethod->id;
+            }
+        }
+    }
+    
+    return $twig->render('Novalnet::NovalnetPaymentMethodReinitializePaymentScript', ['mopIds' => ['paymentMethodId' => $paymentMethodIds]]);
   }
 }
